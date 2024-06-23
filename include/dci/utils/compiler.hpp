8d81034112894a527b02884fe3902767e77879ed
vopl <vopl@bk.ru>
2024-06-23 21:28:09 +0300

    cosmetic

--- include/dci/utils/compiler.hpp
+++ include/dci/utils/compiler.hpp
@@ -10,10 +10,21 @@
 #define likely(x) (x)
 #define unlikely(x) (x)
 
+//TODO: use std::unreachable instead
 #define unreacheable() dci::utils::unreacheableImpl()
 
 namespace dci::utils
 {
-    [[noreturn]] inline void unreacheableImpl() {struct {} r; throw r;}
+    [[noreturn]] inline void unreacheableImpl()
+    {
+        // Uses compiler specific extensions if possible.
+        // Even if no extension is used, undefined behavior is still raised by
+        // an empty function body and the noreturn attribute.
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+        __assume(false);
+#else // GCC, Clang
+        __builtin_unreachable();
+#endif
+    }
 }
 
